9bcc88c1f1a406163bf654fb8131e0c9eefd0cd5
ChristianBloemhof20 <ChristianGBloemhof@gmail.com>
2021-04-04 20:59:15 -0700

    Final application

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,25 +1,9 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -27,6 +11,32 @@ function MyApp() {
     });
     setCharacters(updated);
   }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  return (
+    <div className="container">
+      <div className="container">
+        <Table
+          characterData={characters}
+          removeCharacter={removeOneCharacter}
+        />
+        <Form handleSubmit={updateList} />
+      </div>
+      <p>
+        Credit to{" "}
+        <a
+          href="https://dev.to/dcodeyt/creating-beautiful-html-tables-with-css-428l"
+          target="_blank"
+        >
+          Dom at Dev.to
+        </a>{" "}
+        for my table looking nice.
+      </p>
+    </div>
+  );
 }
 
 export default MyApp;
