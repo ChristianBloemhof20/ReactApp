a846c5cab5cbbbcb0590596da1852ff78837fa71
ChristianBloemhof20 <ChristianGBloemhof@gmail.com>
2021-04-04 20:38:53 -0700

    .

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,9 +1,25 @@
 import React, { useState } from "react";
 import Table from "./Table";
-import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([]);
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -11,13 +27,6 @@ function MyApp() {
     });
     setCharacters(updated);
   }
-
-  return (
-    <div className="container">
-      <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <Form />
-    </div>
-  );
 }
 
 export default MyApp;
