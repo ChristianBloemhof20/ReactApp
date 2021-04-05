c8493d312df3c66fb6b8bde7473d682c44860cb6
ChristianBloemhof20 <ChristianGBloemhof@gmail.com>
2021-04-04 20:18:38 -0700

    Changed the hardcoding to input

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,25 +1,9 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -31,16 +15,7 @@ function MyApp() {
   return (
     <div className="container">
       <Table characterData={characters} removeCharacter={removeOneCharacter} />
-      <p>
-        Credit to{" "}
-        <a
-          href="https://dev.to/dcodeyt/creating-beautiful-html-tables-with-css-428l"
-          target="_blank"
-        >
-          Dom at Dev.to
-        </a>{" "}
-        for my table looking nice.
-      </p>
+      <Form />
     </div>
   );
 }
