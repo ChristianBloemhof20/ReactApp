faab5434e03ecf64c29f1b3b9f440a2126556fd5
Christian <ChristianGBloemhof@gmail.com>
2021-04-11 15:54:39 -0700

    Added backend updates to posting

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,10 +1,27 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 import axios from "axios";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+      fetchAll().then((result) => {
+        if (result) setCharacters(result);
+      });
+    }, []);
+
+  async function fetchAll() {
+    try {
+      const response = await axios.get("http://localhost:5000/users");
+      return response.data.users_list;
+    } catch (error) {
+      //We're not handling errors. Just logging into the console.
+      console.log(error);
+      return false;
+    }
+  }
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -15,6 +32,22 @@ function MyApp() {
 
   function updateList(person) {
     setCharacters([...characters, person]);
+  }
+
+  async function makePostCall(person) {
+    try {
+      const response = await axios.post("http://localhost:5000/users", person);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  }
+
+  function updateList(person) {
+    makePostCall(person).then((result) => {
+      if (result) setCharacters([...characters, person]);
+    });
   }
 
   return (
